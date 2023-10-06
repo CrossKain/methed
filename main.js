3e9a3140d5aec0bb6e42fb81fe4e7597fc6d834b
Denis <densweetfit@gmail.com>
2023-10-06 14:59:26 +0500

    header

--- main.js
+++ main.js
@@ -1,30 +1,82 @@
 import "normalize.css";
 import './style.scss';
-import { Navigation, Thumbs } from 'swiper/modules';
-import Swiper from 'swiper';
-import 'swiper/css';
-
-const swiperThumbnails = new Swiper(".product__slider-thumbnails", {
-  loop: true,
-  spaceBetween: 10,
-  slidesPerView: 4,
-  freeMode: true,
-  watchSlidesProgress: true,
-});
-
-new Swiper(".product__slider-main", {
-  loop: true,
-  spaceBetween: 10,
-  navigation: {
-    nextEl: ".product__arrow-next",
-    prevEl: ".product__arrow-prev",
-  },
-  modules: [Navigation, Thumbs],
-  thumbs: {
-    swiper: swiperThumbnails,
-  },
-});
+import Navigo from "navigo";
+import { Header } from './modules/Header/Header';
 
 
 
 
+const productSlider = () => {
+  Promise.all([
+    import("swiper/modules"),
+    import("swiper"),
+    import("swiper/css"),
+
+  ]).then(([{ Navigation, Thumbs }, Swiper]) => {
+    const swiperThumbnails = new Swiper.default(".product__slider-thumbnails", {
+      loop: true,
+      spaceBetween: 10,
+      slidesPerView: 4,
+      freeMode: true,
+      watchSlidesProgress: true,
+    });
+
+    new Swiper.default(".product__slider-main", {
+      loop: true,
+      spaceBetween: 10,
+      navigation: {
+        nextEl: ".product__arrow-next",
+        prevEl: ".product__arrow-prev",
+      },
+      modules: [Navigation, Thumbs],
+      thumbs: {
+        swiper: swiperThumbnails,
+      },
+    });
+
+  })
+
+}
+
+
+
+const init = () => {
+
+  new Header().mount();
+
+
+
+  productSlider();
+
+  const router = new Navigo('/', { linksSelector: 'a[href^="/"]' });
+
+  router
+    .on("/", () => {
+      console.log('на главной');
+    })
+    .on('/category', () => {
+      console.log('category')
+    })
+    .on('/favorite', () => {
+      console.log('favorite')
+    })
+    .on('/search', () => {
+      console.log('search')
+    })
+    .on('/product/:id', (obj) => {
+      console.log('obj: ', obj);
+
+    })
+    .on('/cart', () => {
+      console.log('cart')
+    })
+    .on('/order', () => {
+      console.log('order')
+    })
+    .notFound(() => {
+      document.body.innerHTML = "<h2>Страница не найдена</h2>";
+    });
+
+  router.resolve();
+};
+init();
