2f57b2f3222e6fe1eb5d08d2a32244515891216f
Denis <densweetfit@gmail.com>
2023-10-07 10:26:58 +0500

    3

--- main.js
+++ main.js
@@ -2,6 +2,8 @@ import "normalize.css";
 import './style.scss';
 import Navigo from "navigo";
 import { Header } from './modules/Header/Header';
+import { Main } from "./modules/Main/Main";
+import { Footer } from "./modules/Footer/Footer";
 
 
 
@@ -43,6 +45,8 @@ const productSlider = () => {
 const init = () => {
 
   new Header().mount();
+  new Main().mount();
+  new Footer().mount();
 
 
 
